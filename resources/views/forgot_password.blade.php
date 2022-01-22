2cbb82bf6d13d6f6997727f0503a0c2211c5d4bc
unknown <hendragunawan6121998@gmail.com>
2022-01-22 18:10:02 +0700

    menambahkan api

--- resources/views/forgot_password.blade.php
+++ resources/views/forgot_password.blade.php
@@ -58,6 +58,7 @@
 <table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF;" width="100%">
 <tbody>
 <tr>
+	<!--ada -->
 <td>
 <table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
 <tbody>
